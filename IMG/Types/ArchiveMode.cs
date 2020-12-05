d77be06dd03e90e9ac9c15603796401603e2433a
BlackMesaDude <francescomesianodev@gmail.com>
2020-12-05 16:17:40 +0100

    Added ArchiveMode.cs

---
+++ IMG/Types/ArchiveMode.cs
@@ -0,0 +1,26 @@
+/*
+    Copyright (C) 2020 BlackMesaDude
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+*/
+
+namespace SATools.IMG.Types
+{
+    /// <summary>
+    /// Defines every available mode that defines how to access the file
+    /// </summary>
+    public enum ArchiveMode 
+    {
+        Read,
+        Create,
+        Update
+    }
+}
