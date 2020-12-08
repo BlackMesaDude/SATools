561e4e4b1fd0bbd5be201362a5610184628512ea
BlackMesaDude <francescomesianodev@gmail.com>
2020-12-08 19:39:52 +0100

    Added additional documentation

--- IMG/Handlers/CloseupArchiveEntryHandler.cs
+++ IMG/Handlers/CloseupArchiveEntryHandler.cs
@@ -1,4 +1,9 @@
 namespace SATools.IMG.Handlers
 {
+    /// <summary>
+    /// Delegate used to handle closeup event for the ArchiveEntryStream.
+    /// </summary>
+    /// <param name="entry">Archive entry that is being closed up</param>
+    /// <param name="stream">Stream that is manipulating the entry</param>
     public delegate void CloseupArchiveEntryHandler(Types.ArchiveEntry entry, Utility.IO.ArchiveEntryStream stream);
 }
