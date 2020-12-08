ed02fcf3eec935dcea26beb6e2133a86e1f01daf
BlackMesaDude <francescomesianodev@gmail.com>
2020-12-08 19:42:41 +0100

    Added additional documentation

--- IMG/Utility/IO/ArchiveEntryStream.cs
+++ IMG/Utility/IO/ArchiveEntryStream.cs
@@ -28,50 +28,101 @@ namespace SATools.IMG.Utility.IO
         /// </summary>
         public event Handlers.CloseupArchiveEntryHandler OnCloseup; 
 
+        /// <summary>
+        /// Creates a ArchiveEntryStream for the specified archive entry
+        /// </summary>
+        /// <param name="target">Target entry to manipulate</param>
+        /// <returns></returns>
         public ArchiveEntryStream(Types.ArchiveEntry target) : base() 
         {
             this._targetEntry = target;
         }
 
+        /// <summary>
+        /// Creates a ArchiveEntryStream for the specified archive entry with a base buffer
+        /// </summary>
+        /// <param name="target">Target entry to manipulate</param>
+        /// <param name="buffer">Base buffer that this stream can use to store the target data</param>
+        /// <returns></returns>
         public ArchiveEntryStream(Types.ArchiveEntry target, byte[] buffer) : base(buffer)
         {
             this._targetEntry = target;
         }
 
+        /// <summary>
+        /// Creates a ArchiveEntryStream for the specified archive entry based on a total capacity
+        /// </summary>
+        /// <param name="target">Target entry to manipulate</param>
+        /// <param name="capacity">Maximum capacity of this stream</param>
+        /// <returns></returns>
         public ArchiveEntryStream(Types.ArchiveEntry target, int capacity) : base(capacity)
         {
             this._targetEntry = target;
         }
 
+        /// <summary>
+        /// Creates a ArchiveEntryStream for the specified archive entry, with a base buffer
+        /// </summary>
+        /// <param name="target">Target entry to manipulate</param>
+        /// <param name="buffer">Base buffer that this stream can use to store target data</param>
+        /// <param name="writeable">Is this stream writeable?</param>
+        /// <returns></returns>
         public ArchiveEntryStream(Types.ArchiveEntry target, byte[] buffer, bool writeable) : base(buffer, writeable)
         {
             this._targetEntry = target;
         }
 
+        /// <summary>
+        /// Creates a ArchiveEntryStream for the specified archive entry, with a base buffer, starting index and maximum count
+        /// </summary>
+        /// <param name="target">Target entry to manipulate</param>
+        /// <param name="buffer">Base buffer that this stream can use to store target data</param>
+        /// <param name="index">Starting index of the stream</param>
+        /// <param name="count">Maximum count for this stream</param>
+        /// <returns></returns>
         public ArchiveEntryStream(Types.ArchiveEntry target, byte[] buffer, int index, int count) : base(buffer, index, count)
         {
             this._targetEntry = target;
         }
 
+        /// <summary>
+        /// Creates a ArchiveEntryStream for the specified archive entry, with a base buffer, strating index and maximum count
+        /// </summary>
+        /// <param name="target">Target entry to manipulate</param>
+        /// <param name="buffer">Base buffer that this stream can use to store target data</param>
+        /// <param name="index">Starting index of the stream</param>
+        /// <param name="count">Maximum count for this stream</param>
+        /// <param name="writeable">Is this stream writeable?</param>
+        /// <returns></returns>
         public ArchiveEntryStream(Types.ArchiveEntry target, byte[] buffer, int index, int count, bool writeable) : base(buffer, index, count, writeable)
         {
             this._targetEntry = target;
         }
 
+        /// <summary>
+        /// Creates a ArchiveEntryStream for the specified archive entry, with a base buffer, strating index and maximum count
+        /// </summary>
+        /// <param name="target">Target entry to manipulate</param>
+        /// <param name="buffer">Base buffer that this stream can use to store target data</param>
+        /// <param name="index">Starting index of the stream</param>
+        /// <param name="count">Maximum count for this stream</param>
+        /// <param name="writeable">Is this stream writeable?</param>
+        /// <param name="visible">Enables GetBuffer() that allows to retrive the unsigend byte buffer where the flux has been created</param>
+        /// <returns></returns>
         public ArchiveEntryStream(Types.ArchiveEntry target, byte[] buffer, int index, int count, bool writeable, bool visible) : base(buffer, index, count, writeable, visible)
         {
             this._targetEntry = target;
         }
 
         /// <summary>
-        /// Closes this entry stream
+        /// Closes this entry stream based on the closeup event that has been defined
         /// </summary>
         public override void Close() 
         {
             if(OnCloseup != null) OnCloseup(_targetEntry, this);
             OnCloseup = null;
 
-            base.Close();
+            base.Close(); // closes the memory stream
         }
     }
 }
